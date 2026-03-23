2f96697611455a1b3d83b687db37bab7920142a6
zencefilefendi <zencefilefendi@users.noreply.github.com>
2026-03-23 04:23:38 +0300

    feat: Convert to Offline Progressive Web App (PWA)

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'diyanet-offline-v1';
+const ASSETS = [
+  './Diyanet_Offline_Motor.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    }).catch(() => {
+      return caches.match('./Diyanet_Offline_Motor.html');
+    })
+  );
+});
